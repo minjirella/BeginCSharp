8d492aaac09f9b7050554f6694fd4b7ab33550c5
minjirella <dyst0410@gmail.com>
2019-04-02 10:16:32 +0900

    new branch

---
+++ baseballcsharp/BaseBall/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseBall
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+
+            //알고리즘 기술 단계
+            // 1 정답을 생성
+            int answer1;
+            int answer2;
+            int answer3;
+            // - while
+            // 2 추측 수를 호출 -> 입력받는다
+            // 3 정답과 추측을 비교하여 결과를 계산
+            // 3-1 정답 숫자와 같을경우 Ball, 정답 숫자에 indexNum까지 맞을 경우 Strike, 아웃
+            // 4 결과를 출력
+            // 5 정답과 추측이 일치하지 않으면 2번으로 돌아간다.
+            // 5-1 정답의 조건 : 3 Strike
+            // cnt 추가해서 횟수 세기
+
+
+        }
+    }
+}
