90ded80adc95413e843b227fbbc38eabe4cf7d64
minjirella <dyst0410@gmail.com>
2019-05-02 19:34:24 +0900

    추가

---
+++ Interface/Interface/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface
+{
+    class Program : InterfaceTest
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Interface");
+            InterfaceTest t = new Program();
+            t.test();
+        }
+
+        public void test()
+        {
+            Console.WriteLine("test");
+        }
+
+        //private static void test()
+        //{
+        //    Console.WriteLine("test");
+        //}
+    }
+}
